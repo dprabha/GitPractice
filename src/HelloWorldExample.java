5972530eaab5446d1cf8a0b99abcaaa9ab8b465f
Dev <devmaheshwar86@gmail.com>
2016-11-13 23:55:01 +0530

    @13Nov16 11:54 PM master

--- src/HelloWorldExample.java
+++ src/HelloWorldExample.java
@@ -3,5 +3,6 @@ public class HelloWorldExample {
 	public static void main(String[] args) {
 		
 		System.out.println("Changed @ 13Nov16 10:29 PM");
+		System.out.println("This is a change in the master branch to induce a merge change ");
 	}
 }
